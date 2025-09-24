6557ac2b0337f0d3fed432df74c66e2b3c9f8b30
jackvannice <jackvannice7@gmail.com>
2025-09-23 19:44:57 -0600

    Second attempt, seems to be working properly, could update to create dynamic course IDs in the input statement

--- Module 7 CT7.py
+++ Module 7 CT7.py
@@ -1,5 +1,5 @@
-def courseInfo():
-    roomNum = {"CSC101":3004,
+def course_info(student_input):
+    room_num = {"CSC101":3004,
                "CSC102":4501,
                "CSC103":6755,
                "NET110":1244,
@@ -9,12 +9,22 @@ def courseInfo():
                "CSC103":"Rich",
                "NET110":"Burke",
                "COM241":"Lee"}
-    meetingTime = {"CSC101":"8:00 am",
+    meeting_time = {"CSC101":"8:00 am",
                "CSC102":"9:00 am",
                "CSC103":"10:00 am",
                "NET110":"11:00 am",
                "COM241":"1:00 pm"}
-    return roomNum, instructors, meetingTime
+    if student_input in room_num:
+        return room_num[student_input], instructors[student_input], meeting_time[student_input]
+    else:
+        return "", "", ""
 
-student_input = ("Please input your course number: ")
-course_info = courseInfo(student_input)
+student_input = input("Please input your course number (options: CSC101, CSC102, CSC103, NET110, COM241): ")
+course_info = course_info(student_input)
+
+if course_info[0] != "":
+    print(f"Room Number: {course_info[0]}")
+    print(f"Instructor: {course_info[1]}")
+    print(f"Meeting Time: {course_info[2]}")
+else:
+    print("Room Number not found")
